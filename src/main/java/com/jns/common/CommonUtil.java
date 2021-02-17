d044adb660fde2cc95d6ef8acbff684048a0c57a
fullsleep <fullsleeep@gmail.com>
2021-02-17 19:12:19 +0900

    chefboard 수정

--- src/main/java/com/jns/common/CommonUtil.java
+++ src/main/java/com/jns/common/CommonUtil.java
@@ -10,6 +10,6 @@ public class CommonUtil {
 	
 	
 	//파일 업로드 
-	public static final String FILE_UPLOAD_PATH = "";
+	public static final String FILE_UPLOAD_PATH = "C:\\00.KOSMO72";
 	public static final int FILE_FILE_SIZE = 10*1024*1024; //10MB	
 }
