c2f22647f37ce4fbfe036dfdedf9b5b9f9aadb4e
GeoYeo-DreamTree <jhc52099@naver.com>
2021-02-25 20:27:38 +0900

    영양소VO 구현

---
+++ src/main/java/com/jns/flask/controller/FlaskController.java
@@ -0,0 +1,11 @@
+package com.jns.flask.controller;
+
+import com.jns.flask.vo.NutrientVO;
+
+public class FlaskController 
+{
+	public static void main(String[] args) 
+	{
+		new NutrientVO("445", "70", "26", "7", "245");
+	}
+}
